095b83717c2349707e25a059784cca1e821627de
Kent Fredric <kentfredric@gmail.com>
2014-06-11 15:24:36 +1200

    Mock design

--- lib/Test/WithNetwork.pm
+++ lib/Test/WithNetwork.pm
@@ -3,10 +3,63 @@ use warnings;
 
 package Test::WithNetwork;
 
-# ABSTRACT:
+# ABSTRACT: Indicate a test requires Network resources to work.
 
-use Moo;
+=head1 SYNOPSIS
 
-no Moo;
+=head2 In Tests
+
+=head3 Per-File Filter
+
+  use Test::More;
+  use Test::WithNetwork 'Dist-Foo-Bar' => [qw( dns tcp ssl )]; # exit skip if policy doesn't indicate all are permitted.
+
+=head3 Localised Filter
+
+  use Test::More;
+  use Test::WithNetwork 'Dist-Foo-Bar';
+
+  with_network [qw( dns tcp )] => sub {
+    # Tests that require network resources
+
+  };
+  # Non-network depedndent tests
+
+=head3 Conditional Checking
+
+  use Test::More;
+  use Test::WithNetwork 'Dist-Foo-Bar';
+
+  if ( can_network(qw( dns tcp )) ) {
+    # Tests that require network resources
+
+  };
+  # Non-network depedndent tests
+
+=cut
+
+=head2 User Side
+
+C<~/.perl_test_withnetwork.ini>
+
+  -permission_denied = silent
+  ; -permission_denied = warn
+  ; -permission_denied = log
+  ; -log_file = /tmp/test-with-network.log
+  dns = y
+  tcp = n
+  ssl = n
+
+  # Special grants for Dist-Foo-Bar
+  [Dist-Foo-Bar]
+  # dns inherited from global
+  tcp = y
+
+=head2 Standard Test output
+
+  t/01-somenetworkthing: Test::WithNetwork was not granted Dist-Foo-Bar/ssl permission. Skipped.
+  t/02-somenetworkthing: Test::WithNetwork was not granted Dist-Foo-Bar/ssl permission. Skipped some tests.
+
+=cut
 
 1;
